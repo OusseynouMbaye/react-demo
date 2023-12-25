0401bda1dc658b4749ece1f3acd93cfa5a5519c4
Ouzinem <ouzinem@hotmail.com>
2023-12-25 10:30:47 -0500

    config decembre23

--- src/Components/Formulaire.jsx
+++ src/Components/Formulaire.jsx
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import Navigation from "./Navigation.jsx";
 
 const Formulaire = () => {
+  const [firstname, setFirstname] = useState("ouzinem");
+
+  const handleChange = (e) => {
+    setFirstname(e.target.value);
+  };
+  const reset = () => {
+    setFirstname("");
+  };
+
+  console.log("render");
+
   return (
     <>
       <Navigation />
       <h1>Formulaire</h1>
+      <form>
+        <input
+          type="text"
+          name="firstname"
+          value={firstname}
+          onChange={handleChange}
+        />
+        {firstname}
+        <button onClick={reset} type="button">
+          reset
+        </button>
+      </form>
     </>
   );
 };
